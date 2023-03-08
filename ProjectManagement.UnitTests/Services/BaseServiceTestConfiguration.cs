8374bc8a323927fdd1a93b650bb79a6a3c7357a0
cariander <carianderrr@gmail.com>
2023-03-08 17:29:10 +0600

    Create BaseServiceTestConfiguration

---
+++ ProjectManagement.UnitTests/Services/BaseServiceTestConfiguration.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using ProjectManagement.BAL.MappingProfiles;
+using ProjectManagement.DAL.Contracts;
+
+namespace ProjectManagement.UnitTests.Services;
+
+public class BaseServiceTestConfiguration
+{
+    protected readonly IConfiguration Configuration;
+    protected readonly IMapper Mapper;
+    protected readonly IProjectRepository ProjectRepository;
+    protected readonly IEmployeeRepository EmployeeRepository;
+
+    protected BaseServiceTestConfiguration()
+    {
+        Mapper = new MapperConfiguration(cfg => { cfg.AddMaps(typeof(ProjectProfile)); }).CreateMapper();
+        Configuration = new ConfigurationBuilder().Build();
+        ProjectRepository = Substitute.For<IProjectRepository>();
+        EmployeeRepository = Substitute.For<IEmployeeRepository>();
+    }
+}

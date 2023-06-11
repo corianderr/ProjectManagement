7f398081e487c165dea85d2efed8c5cede1e99b2
cariander <carianderrr@gmail.com>
2023-06-11 18:15:43 +0600

    Add update project's model validation

--- ProjectManagement.BAL/Models/Project/UpdateProjectModel.cs
+++ ProjectManagement.BAL/Models/Project/UpdateProjectModel.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjectManagement.BAL.Models.Project;
 
 public class UpdateProjectModel
 {
+    [Required(ErrorMessage = "Please enter your name using less than 50 characters."), StringLength(50)]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter client company's name."), StringLength(50)]
     public string ClientCompanyName { get; set; }
+    [Required(ErrorMessage = "Please enter executor company's name."), StringLength(50)]
     public string ExecutorCompanyName { get; set; }
+    [Range(1, 4, ErrorMessage = "Priority value must be between 1 and 3")]
+    [Required(ErrorMessage = "Please enter priority.")]
     public int Priority { get; set; }
+    [Required(ErrorMessage = "Please enter manager's id.")]
     public int ManagerId { get; set; }
 }

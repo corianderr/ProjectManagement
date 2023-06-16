ac2390eb1eea5cc49c73658677eaffdefb45b1c4
cariander <carianderrr@gmail.com>
2023-06-16 18:09:42 +0600

    Implement UI to filter projects

--- ProjectManagement.API/ClientApp/src/components/ProjectsCRUD.js
+++ ProjectManagement.API/ClientApp/src/components/ProjectsCRUD.js
@@ -8,19 +8,33 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import dayjs from "dayjs";
 
 const ProjectsCRUD = () => {
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState([]);
     const [show, setShow] = useState(false);
     const [editShow, setEditShow] = useState(false);
-    const [form, setForm] = useState({name: "", clientCompanyName: "", executorCompanyName: "", priority: 0, managerId: 0});
+    const [form, setForm] = useState({
+        name: "",
+        clientCompanyName: "",
+        executorCompanyName: "",
+        priority: 0,
+        managerId: 0
+    });
+    const [filter, setFilter] = useState({
+        name: "",
+        priority: null,
+        startDateFrom: null,
+        startDateTo: null,
+        orderBy: ""
+    });
     const [employees, setEmployees] = useState(null);
-    
+
     const handleClose = () => setShow(false);
-   
-    const handleEditClose = () => setShow(false);
-    
+
+    const handleEditClose = () => setEditShow(false);
+
     useEffect(() => {
         try {
             fetchData().catch((e) => console.log())
@@ -29,13 +43,25 @@ const ProjectsCRUD = () => {
             setLoading(false)
         }
     }, [])
-        
-    
+
+
     const handleShow = () => {
         setShow(true)
         getEmployees().catch((e) => console.log());
     }
-    
+
+    const handleEditShow = async (id) => {
+        await axios(`api/projects/getById/${id}`)
+            .then((result) => {
+                getEmployees();
+                setForm(result.data.result);
+                setEditShow(true)
+            })
+            .catch(error => {
+                toast.error(error)
+            })
+    }
+
     const getEmployees = async () => {
         await axios('api/employees')
             .then((result) => {
@@ -54,19 +80,7 @@ const ProjectsCRUD = () => {
         setData(data.result)
         setLoading(false)
     }
-    
-    const handleEditShow = async (id) => {
-        await axios(`api/projects/getById/${id}`)
-            .then((result) => {
-                getEmployees();
-                setForm(result.data.result);
-                setEditShow(true)
-            })
-            .catch(error => {
-                toast.error(error)
-            })
-    }
-    
+
     const handleEdit = (e, id) => {
         e.preventDefault()
         axios.put(`api/projects/${id}`, form)
@@ -84,7 +98,7 @@ const ProjectsCRUD = () => {
         if (window.confirm("Are you sure you want to delete this project?")) {
             axios.delete(`api/projects/${id}`)
                 .then((result) => {
-                    if (result.status === 200){
+                    if (result.status === 200) {
                         toast.success('Project has been deleted');
                         fetchData().catch(() => console.log());
                     }
@@ -100,6 +114,11 @@ const ProjectsCRUD = () => {
         setForm(prev => ({...prev, [name]: value}));
     };
 
+    const onChangeFilter = e => {
+        const {name, value} = e.target;
+        setFilter(prev => ({...prev, [name]: value}));
+    };
+
     const handleAdd = (e) => {
         e.preventDefault()
         axios.post('api/projects', form)
@@ -112,65 +131,92 @@ const ProjectsCRUD = () => {
             console.log(error.response.data.errors)
         })
     }
-    
+
+    const handleFilter = async (e) => {
+        e.preventDefault();
+        const dataFrom = filter.startDateFrom == null ? "" : `&startDateFrom=${filter.startDateFrom}`;
+        const dataTo = filter.startDateTo == null ? "" : `&startDateTo=${filter.startDateTo}`;
+        const priority = filter.priority == null ? "" : `&priority=${filter.priority}`;
+        const url = `api/projects?name=${filter.name}` + priority + dataFrom + dataTo + `&orderBy=${filter.orderBy}`;
+        await axios(url)
+            .then((result) => {
+                setData(result.data.result);
+            }).catch((error) => {
+                toast.error(error)
+            })
+    }
+
     const clear = () => {
         setForm({name: "", clientCompanyName: "", executorCompanyName: "", priority: 0, managerId: 0})
     }
-    
+
+    console.log(filter)
+
     return (
         <div>
             <ToastContainer/>
             {
                 loading ? <p><em>Loading...</em></p> :
                     <div>
-                        <Form>
-                            <Row>
-                                <Col>
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control placeholder="Peter" />
-                                </Col>
-                                <Col>
-                                    <Form.Label>Priority</Form.Label>
-                                    <Form.Control placeholder="1" type="number" min={0} max={3}/>
-                                </Col>
-                                <Col>
-                                    <Form.Label>Start Date From</Form.Label>
-                                    <Form.Control type="date"/>
-                                </Col>
-                                <Col>
-                                    <Form.Label>Start Date To</Form.Label>
-                                    <Form.Control type="date"/>
-                                </Col>
-                                <Col>
-                                    <Button className="ms-auto w-100" variant="none" onClick={handleShow}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
-                                             fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                                            <path
-                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                                        </svg></Button>
-                                </Col>
-                            </Row>
-                        </Form>
-                <table className='table table-striped text-center' aria-labelledby="tabelLabel">
-                    <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((project, index) => <tr key={index}>
-                        <td>{project.id}</td>
-                        <td><Link tag={Link} className="text-dark"
-                                  to={`/getById/${project.id}`}>{project.name}</Link></td>
-                        <td>
-                            <button className="btn btn-primary px-4 me-2" onClick={() => handleEditShow(project.id)}>Edit</button>
-                            <button className="btn btn-danger px-4 ms-2" onClick={() => handleDelete(project.id)}>Delete</button>
-                        </td>
-                    </tr>)}
-                    </tbody>
-                </table>
+                        <Row>
+                            <Col>
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control value={filter.name} name="name" onChange={onChangeFilter}/>
+                            </Col>
+                            <Col>
+                                <Form.Label>Priority</Form.Label>
+                                <Form.Control name="priority" type="number"
+                                              min={0} max={3} onChange={onChangeFilter}/>
+                            </Col>
+                            <Col>
+                                <Form.Label>Start Date From</Form.Label>
+                                <Form.Control name="startDateFrom" type="date"
+                                              onChange={onChangeFilter}/>
+                            </Col>
+                            <Col>
+                                <Form.Label>Start Date To</Form.Label>
+                                <Form.Control name="startDateTo" type="date"
+                                              onChange={onChangeFilter}/>
+                            </Col>
+                            <Col>
+                                <div className="h-100 d-flex">
+                                    <Button className="mt-auto mb-2 px-4" variant="secondary" onClick={(e) => handleFilter(e)}>Filter</Button>
+                                </div>
+                            </Col>
+                            <Col>
+                                <Button className="ms-auto w-100 opacity-25" variant="none" onClick={handleShow}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
+                                         fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                                    </svg>
+                                </Button>
+                            </Col>
+                        </Row>
+                        <table className='table table-striped text-center' aria-labelledby="tabelLabel">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {data.map((project, index) => <tr key={index}>
+                                <td>{project.id}</td>
+                                <td><Link tag={Link} className="text-dark"
+                                          to={`/getById/${project.id}`}>{project.name}</Link></td>
+                                <td>
+                                    <button className="btn btn-primary px-4 me-2"
+                                            onClick={() => handleEditShow(project.id)}>Edit
+                                    </button>
+                                    <button className="btn btn-danger px-4 ms-2"
+                                            onClick={() => handleDelete(project.id)}>Delete
+                                    </button>
+                                </td>
+                            </tr>)}
+                            </tbody>
+                        </table>
                     </div>
             }
             <Modal show={show} onHide={handleClose}>
@@ -180,24 +226,32 @@ const ProjectsCRUD = () => {
                 <Modal.Body>
                     <form onSubmit={(e) => handleAdd(e)}>
                         <div className="pb-3">
-                            <input value={form.name} name="name" type="text" className="form-control" 
+                            <input value={form.name} name="name" type="text" className="form-control"
                                    placeholder="Enter name" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.clientCompanyName} name="clientCompanyName" type="text" className="form-control" 
+                            <input value={form.clientCompanyName} name="clientCompanyName" type="text"
+                                   className="form-control"
                                    placeholder="Enter client's company name" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.executorCompanyName} name="executorCompanyName" type="text" 
-                                   className="form-control" placeholder="Enter executor's company name" onChange={onChange} required/>
+                            <input value={form.executorCompanyName} name="executorCompanyName" type="text"
+                                   className="form-control" placeholder="Enter executor's company name"
+                                   onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.priority} name="priority" type="number" min={0} max={3} 
+                            <input value={form.priority} name="priority" type="number" min={0} max={3}
                                    className="form-control" placeholder="Enter priority" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.managerId} name="managerId" type="number" min={0} max={3} 
-                                   className="form-control" placeholder="Choose manager id" onChange={onChange} required/>
+                            {
+                                employees != null ?
+                                    <Form.Select name="managerId" onChange={onChange}>{
+                                        employees.map((id) =>
+                                            <option key={id} value={id}>{id}</option>)
+                                    }</Form.Select>
+                                    : <p></p>
+                            }
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
@@ -214,26 +268,33 @@ const ProjectsCRUD = () => {
                                    placeholder="Enter name" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.clientCompanyName} name="clientCompanyName" type="text" className="form-control"
+                            <input value={form.clientCompanyName} name="clientCompanyName" type="text"
+                                   className="form-control"
                                    placeholder="Enter client's company name" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
                             <input value={form.executorCompanyName} name="executorCompanyName" type="text"
-                                   className="form-control" placeholder="Enter executor's company name" onChange={onChange} required/>
+                                   className="form-control" placeholder="Enter executor's company name"
+                                   onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
                             <input value={form.priority} name="priority" type="number" min={0} max={3}
                                    className="form-control" placeholder="Enter priority" onChange={onChange} required/>
                         </div>
                         <div className="pb-3">
-                            <input value={form.managerId} name="managerId" type="number" min={0} max={3}
-                                   className="form-control" placeholder="Choose manager id" onChange={onChange} required/>
+                            {
+                                employees != null ?
+                                    <Form.Select name="managerId" onChange={onChange}>{
+                                        employees.map((id) =>
+                                            <option key={id} value={id}>{id}</option>)
+                                    }</Form.Select>
+                                    : <p></p>
+                            }
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </Modal.Body>
             </Modal>
-
         </div>);
 }
 

9e4d435095bd6ae665b6597abb26511c18851ae5
cariander <carianderrr@gmail.com>
2023-06-18 03:20:20 +0600

    Custom home page

--- ProjectManagement.API/ClientApp/src/components/Home.js
+++ ProjectManagement.API/ClientApp/src/components/Home.js
@@ -5,34 +5,46 @@ export class Home extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Hello, world!</h1>
-                <p>Welcome to your new single-page application, built with:</p>
-                <ul>
-                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a
-                        href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform
-                        server-side code
-                    </li>
-                    <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-                </ul>
-                <p>To help you get started, we have also set up:</p>
-                <ul>
-                    <li><strong>Client-side navigation</strong>. For example,
-                        click <em>Counter</em> then <em>Back</em> to return here.
-                    </li>
-                    <li><strong>Development server integration</strong>. In development mode, the development server
-                        from <code>create-react-app</code> runs in the background automatically, so your client-side
-                        resources are dynamically built on demand and the page refreshes when you modify any file.
-                    </li>
-                    <li><strong>Efficient production builds</strong>. In production mode, development-time features are
-                        disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently
-                        bundled JavaScript files.
-                    </li>
-                </ul>
-                <p>The <code>ClientApp</code> subdirectory is a standard React application based on
-                    the <code>create-react-app</code> template. If you open a command prompt in that directory, you can
-                    run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            <div className="text-center container">
+                <h1>Hi !</h1>
+                <p>(Links are on the top)</p>
+                <br/>
+                <h2 className="text-center">Welcome to Karina's Project Managment App !!</h2>
+                <p>
+                    ProjectManagement is a game-changing project management solution that redefines how you handle
+                    projects. With its cutting-edge three-tier architecture, it empowers you to take control and
+                    drive success. Seamlessly access and manage project data with lightning speed through the Data
+                    Access Level (DAL). Elevate your project's logic and fine-tune every aspect with the Business
+                    Access Level (BAL) and comprehensive unit tests. Achieve seamless integration and collaboration
+                    using the Application Programming Interface (API). Securely store your data with SQLite. Engage
+                    your team with a visually captivating frontend powered by ReactJS. Experience the future of project
+                    management with ProjectManagement and unlock your project's true potential.
+                </p>
+                <br/>
+                <h3>Here are the technologies that were used:</h3>
+                <div className="row m-auto">
+                    <div className="ms-auto col-3">
+                        <p>Backend</p>
+                        <ul className="technologies">
+                            <li>.NET 6</li>
+                            <li>ASP.NET Core 6</li>
+                            <li>Entity Framework Core</li>
+                            <li>AutoMapper</li>
+                            <li>XUnit (Unit tests)</li>
+                            <li>FluentAssertion (Testing project)</li>
+                            <li>NBuilder (Testing project)</li>
+                        </ul>
+                    </div>
+                    <div className="me-auto col-3">
+                        <p>Frontend</p>
+                        <ul className="front-technologies">
+                            <li>ReactJS</li>
+                            <li>React Toastify</li>
+                            <li>React Bootstrap</li>
+                            <li>Axios</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }

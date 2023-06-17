92cd3da0c3343ee6706a3aca0e36c29ad327a9b9
cariander <carianderrr@gmail.com>
2023-06-17 17:03:53 +0600

    Add configuration file for axios

---
+++ ProjectManagement.API/ClientApp/src/axiosApi.js
@@ -0,0 +1,27 @@
+import axios, {isAxiosError} from 'axios';
+import {toast} from "react-toastify";
+
+const axiosApi = axios.create({
+    baseURL: 'api/',
+});
+
+    axiosApi.interceptors.response.use(
+        (response) => response,
+        (error) => {
+
+            if (isAxiosError(error)) {
+
+                console.log(error)
+                if (error.response.status >= 400 && error.response.status < 500) {
+                    console.log(error.response.data.errors)
+
+                    toast.error(JSON.stringify(error.response.data.errors).replace(/[{}[\]"]/g, ' '));
+                } else if (error.response.status >= 500) {
+                    toast.error('test test');
+                }
+                throw error;
+            }
+        }
+    )
+
+export default axiosApi;
